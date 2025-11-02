70e8f12c506bf31c6e6c984f70dcbe8063083753
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-11-02 12:40:39 -0500

    feat(cost calculation per amount of miles): created variables to store the value for the cost in dollars per 20, 75, and 500 miles.

--- Module 1/Lab Activities/GasMileage.cpp
+++ Module 1/Lab Activities/GasMileage.cpp
@@ -9,5 +9,9 @@ float gasMileage = 0;
 cin >> gasMileage;
 cin >> costPerGallon;
 
+float costPerTwentyMiles = (costPerGallon * 20) / gasMileage;
+float costPerSeventyFiveMiles = (costPerGallon * 75) / gasMileage;
+float costPerFiveHundredMiles = (costPerGallon * 500) / gasMileage;
+
    return 0;
 }
