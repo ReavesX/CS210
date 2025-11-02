6011f154e76b6892c334ffd69d0f3e91f4803e6c
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-11-02 10:53:21 -0500

    feat(hello world python): Created hello world alongside an executable made with pyinstaller

---
+++ Module 1/Comaparing Programming Languages/src/HelloWorld.py
@@ -0,0 +1,11 @@
+# Developer: Don Jackson
+# Date: November 2, 2025
+# Purpose: This application prints "Hello, World!" to the console in Python.
+
+def main():
+    print("Hello, World!")  # Output the greeting message to the console
+    wait = input("Press Enter to exit...")  # Wait for user input before closing
+
+if __name__ == "__main__":  # Only run the main if this script is executed directly
+    main()
+    
