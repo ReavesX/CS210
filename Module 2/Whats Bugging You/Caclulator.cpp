2438d7c3825d834937caa44a93a98f598f28e947
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-11-07 18:25:51 -0500

    chore(fix): fixed formatting & updated answer to use single quotes.

--- Module 2/Whats Bugging You/Caclulator.cpp
+++ Module 2/Whats Bugging You/Caclulator.cpp
@@ -1,8 +1,8 @@
 /*
  * Calculator.cpp
  *
- *  Date: [Enter date]
- *  Author: [Your Name]
+ *  Date: 11/7/2025 
+ *  Author: Don Jackson
  */
 
 #include <iostream>
@@ -14,10 +14,10 @@ void main()
 	char statement[100];
 	int op1, op2;
 	char operation;
-	char answer="Y"
-	while (answer=='y')
+	char answer = "Y"; // missing semicolon here & updated formatting
+	while (answer == 'Y') // updated character to be the same as initial answer & updated formatting 
 	{
-		cout << "Enter expression" <<endl;
+		cout << "Enter expression" << endl; // formatting
 		cin >> op2 >> operation >> op1;
 		if (operation == "+");
 			cout << op1 << " + " << op2 >> " = " << op1 + op2 << endl;
