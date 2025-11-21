ac1d1da3a2ea84d6d2b75c230fcb1ea6324565de
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-11-21 16:39:50 -0500

    feat(lab complete): completed lab with formatting using a while loop and a statement.

--- Module 2/Whats Bugging You/Lab Activities/MadLib.cpp
+++ Module 2/Whats Bugging You/Lab Activities/MadLib.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main() {
-   
+    int quantity; 
+    string item;
+    
+    while (cin >> item && item != "quit"){
+        cin >> quantity;
+        cout << "Eating " << quantity << " " << item << " a day keeps you happy and healthy." << endl;
+    }
    
    return 0;
 }
